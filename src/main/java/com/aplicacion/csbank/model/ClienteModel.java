04d19bdfc0209929a910fa0872e9a452da31e009
illancapan <igorllancapan@gmail.com>
2024-09-10 03:07:33 -0300

    Desarrollando model

---
+++ src/main/java/com/aplicacion/csbank/model/ClienteModel.java
@@ -0,0 +1,11 @@
+package com.aplicacion.csbank.model;
+
+public class ClienteModel {
+
+    private String nombre;
+    private String apellido;
+    private String rut;
+    private String correo;
+    private String tarjeta_credito;
+    private Double saldo;
+}
